c5d0d611a6ea71ff54cd6cb714039955fe76154c
Logan Wood <lwood3499@gmail.com>
2019-05-13 23:50:49 -0500

    completed functionality to sort by price or name

---
+++ client/src/components/Plan.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="plan-card" @click="handleClicked">
+		<h1 class="plan-title">{{plan.name}}</h1>
+		<p class="description">{{plan.description}}</p>
+		<h4 class="price">Price: ${{plan.price}}</h4>
+		<h4 class="package-type">Type: {{plan.type}}</h4>
+		<h4 class="section">Section: {{plan.section}}</h4>
+		<h4 v-if="plan.bestSellers" class="best-sellers">**Best Seller**</h4>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+				title: ""
+			};
+		},
+		props: {
+			plan: {
+				type: Object,
+				required: true
+			}
+		},
+		methods: {
+			handleClicked: function() {
+				this.$emit("planID", { planID: this.plan.id });
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	* {
+		font-family: "Trebuchet MS", Helvetica, sans-serif;
+	}
+
+	.plan-card {
+		border: 1px solid rgba(165, 165, 165, 0.575);
+		width: 45%;
+		margin: 10px auto;
+		cursor: pointer;
+	}
+
+	.plan-title {
+		margin-top: 0;
+		padding: 10px;
+		width: 100%;
+		background-color: rgb(36, 152, 123);
+		color: #fff;
+		box-sizing: border-box;
+	}
+
+	.description,
+	.price,
+	.package-type,
+	.section,
+	.best-sellers {
+		padding: 0 10px;
+	}
+</style>
--- client/src/views/QuotesPage.vue
+++ client/src/views/QuotesPage.vue
@@ -1,20 +1,67 @@
 <template>
 	<div>
 		<h1>{{pageTitle}}</h1>
-		<div v-for="quote in quotes" :key="quote.id">
-			<h1>{{quote.name}}</h1>
-			<p>{{quote.description}}</p>
+		<button @click="sort('price')">Sort by Price</button>
+		<button @click="sort('name')">Sort by Name</button>
+		<div v-for="quote in modQuotes" :key="quote.id">
+			<plan :plan="quote" @planID="handleClicked"/>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Plan from "../components/Plan.vue";
+
 	export default {
 		data: function() {
 			return {
 				pageTitle: "Quotes",
-				quotes: []
+				quotes: [],
+				modQuotes: [],
+				showOriginalQuotes: true
 			};
+		},
+		computed: {
+			priceSorted: function() {
+				let newQuotes = [...this.quotes];
+				return newQuotes.sort((a, b) => {
+					// sort by price value
+					if (a.price > b.price) {
+						return 1;
+					}
+					if (a.price < b.price) {
+						return -1;
+					}
+					return 0;
+				});
+			},
+			nameSorted: function() {
+				let newQuotes = [...this.quotes];
+				return newQuotes.sort((a, b) => {
+					// if unicode value of a.name > b.name then return positive for sorting
+					if (a.name > b.name) {
+						return 1;
+					}
+					// unicode value comparison for sorting
+					if (a.name < b.name) {
+						return -1;
+					}
+					// plans have same value so no change
+					return 0;
+				});
+			}
+		},
+		methods: {
+			handleClicked: function(val) {
+				console.log("from quotes-page: ", val.planID);
+			},
+			sort: function(type) {
+				if (type === "price") {
+					this.modQuotes = this.priceSorted;
+				} else if (type === "name") {
+					this.modQuotes = this.nameSorted;
+				}
+			}
 		},
 		created: function() {
 			let vm = this;
@@ -24,13 +71,12 @@
 					return res.json();
 				})
 				.then(function(jsonData) {
-					console.log(jsonData.quotes);
 					vm.quotes = jsonData.quotes;
+					vm.modQuotes = [...vm.quotes];
 				});
-			// .then(function(data) {
-			// 	vm.quotes = data;
-			// 	console.log("data: ", data.body);
-			// });
+		},
+		components: {
+			plan: Plan
 		}
 	};
 </script>
